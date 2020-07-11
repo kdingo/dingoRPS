5f29aca91859e983487658abae8a0e2a581f2711
kdingo <kdingo@gmail.com>
2020-07-10 20:49:53 -0700

    Expanding winning comparison test

--- RPSTest/HandTests.cs
+++ RPSTest/HandTests.cs
@@ -35,13 +35,21 @@ namespace RPSTest {
             //Arrange
             var hand1 = new Hand("Rock");
             var hand2 = new Hand("Scissors");
-            var expected = 1;
+            var hand3 = new Hand("rock");
+            var expectedWin = 1;
+            var expectedLoss = 0;
+            var expectedDraw = -1;
 
             //Act
             var actualHand1 = hand1.IsWinner(hand2);
+            var actualHand2 = hand2.IsWinner(hand1);
+            var actualHand3 = hand1.IsWinner(hand3);
 
             //Assert
-            Assert.AreEqual(actualHand1, expected);
+            Assert.AreEqual(actualHand1, expectedWin);
+            Assert.AreEqual(actualHand2, expectedLoss);
+            Assert.AreEqual(actualHand3, expectedDraw);
+
         }
     }
 }
